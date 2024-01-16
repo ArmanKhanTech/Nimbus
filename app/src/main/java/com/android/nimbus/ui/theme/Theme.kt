076e4a219a8c1ac9359aed090c48e80afe34dcbf
arman <ak2341776@gmail.com>
2024-01-16 21:23:20 +0530

    Initial Commit

--- app/src/main/java/com/android/nimbus/ui/theme/Theme.kt
+++ app/src/main/java/com/android/nimbus/ui/theme/Theme.kt
@@ -26,13 +26,13 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40,
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
+    background = Color.White,
+    surface = Color.White,
     onPrimary = Color.White,
     onSecondary = Color.White,
     onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
+    onBackground = Color.Black,
+    onSurface = Color.Black,
 )
 
 @Composable
